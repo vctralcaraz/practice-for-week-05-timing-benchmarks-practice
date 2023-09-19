33df878d477ff99afa4f3e1b542233b60ce217c5
Victor Alcaraz <vctralcaraz@gmail.com>
2023-09-18 23:00:58 -0700

    phase 3 complete

--- phase-3.js
+++ phase-3.js
@@ -5,7 +5,25 @@ function addNums10Timing(increment) {
   // Then, add timing code
 
   // Your code here
+  let arr = [];
+  let inc = increment;
 
+  console.time('addNums');
+  for(let i = 1; i <= 10; i++) {
+    let startTime = Date.now();
+
+    arr.push(addNums(inc));
+
+    let endTime = Date.now();
+    console.log(`${endTime - startTime}`);
+
+    console.timeLog('addNums');
+
+    inc += increment;
+  }
+  console.timeEnd('addNums');
+
+  return arr;
 }
 
 
@@ -14,7 +32,69 @@ function addManyNums10Timing(increment) {
 // Then, add timing code
 
   // Your code here
+  let arr = [];
+  let inc = increment;
 
+  console.time('addManyNums');
+  for(let i = 1; i <= 10; i++) {
+    let startTime = Date.now();
+
+    arr.push(addManyNums(inc));
+
+    let endTime = Date.now();
+    console.log(`${endTime - startTime}`);
+
+    console.timeLog('addManyNums');
+
+    inc += increment;
+  }
+  console.timeEnd('addManyNums');
+
+  return arr;
+}
+
+function addNums100Timing(increment) {
+  let arr = [];
+  let inc = increment;
+
+  // console.time('addNums');
+  for(let i = 1; i <= 100; i++) {
+    let startTime = Date.now();
+
+    arr.push(addNums(inc));
+
+    let endTime = Date.now();
+    console.log(`${endTime - startTime}`);
+
+    // console.timeLog('addNums');
+
+    inc += increment;
+  }
+  // console.timeEnd('addNums');
+
+  return arr;
+}
+
+function addManyNums100Timing(increment) {
+  let arr = [];
+  let inc = increment;
+
+  // console.time('addManyNums');
+  for(let i = 1; i <= 100; i++) {
+    let startTime = Date.now();
+
+    arr.push(addManyNums(inc));
+
+    let endTime = Date.now();
+    console.log(`${endTime - startTime}`);
+
+    // console.timeLog('addManyNums');
+
+    inc += increment;
+  }
+  // console.timeEnd('addManyNums');
+
+  return arr;
 }
 
 
@@ -27,3 +107,15 @@ console.log("\n***********\n");
 n = 1000
 console.log(`addManyNums(${n}): `);
 addManyNums10Timing(5000);
+
+console.log("\n***********\n");
+
+n = 1000000
+console.log(`addNums100(${n}): `);
+addNums100Timing(1000000);
+
+console.log("\n***********\n");
+
+n = 1000
+console.log(`addManyNums100(${n}): `);
+addManyNums100Timing(5000);
