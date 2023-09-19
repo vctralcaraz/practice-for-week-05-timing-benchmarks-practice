2359b2ea5a3faa36f7ded1a7d1354d0f2f0c0c7f
Victor Alcaraz <vctralcaraz@gmail.com>
2023-09-18 20:25:19 -0700

    phase 2 complete

--- phase-2.js
+++ phase-2.js
@@ -3,13 +3,29 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
+  let arr = [];
+  let inc = increment;
 
+  for(let i = 1; i <= 10; i++) {
+    arr.push(addNums(inc));
+    inc += increment;
+  }
+
+  return arr;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
+  let arr = [];
+  let inc = increment;
 
+  for(let i = 1; i <= 10; i++) {
+    arr.push(addManyNums(inc));
+    inc += increment;
+  }
+
+  return arr;
 }
 
 module.exports = [addNums10, addManyNums10];
